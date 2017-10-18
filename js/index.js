f0de30e23d9663178ceb10697194cb28bbb14e31
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2017-10-18 20:50:19 +0200

    Let's go

---
+++ js/index.js
@@ -0,0 +1,106 @@
+// To you, brave developer, who venture past the "Inspect this page button", know this:
+// I stole this code from https://codepen.io/markhillard/pen/Hjcwu
+// And then I slightly modified it.
+
+// External Files:
+// https://api.html5media.info/1.1.8/html5media.min.js (enables <video> and <audio> tags in all major browsers)
+// https://cdn.plyr.io/2.0.13/plyr.js
+
+
+// HTML5 audio player + playlist controls...
+// Inspiration: http://jonhall.info/how_to/create_a_playlist_for_html5_audio
+// Mythium Archive: https://archive.org/details/mythium/
+jQuery(function ($) {
+    'use strict'
+    var supportsAudio = !!document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+        var index = 0,
+            playing = false,
+            mediaPath = 'https://github.com/aspyct/music/blob/master/media/',
+            tracks = [{
+                "name": "Binary Sunset - Piano Cover",
+                "length": "1:24",
+                "file": "Binary%20Sunset%2018%20Oct%202017.m4a"
+            },
+            {
+                "name": "Into the Cave",
+                "length": "1:52",
+                "file": "Into%20the%20Cave.m4a"
+            }],
+            buildPlaylist = $.each(tracks, function(key, value) {
+                var trackNumber = key + 1,
+                    trackName = value.name,
+                    trackLength = value.length;
+                if (trackNumber.toString().length === 1) {
+                    trackNumber = '0' + trackNumber;
+                } else {
+                    trackNumber = '' + trackNumber;
+                }
+                $('#plList').append('<li><div class="plItem"><div class="plNum">' + trackNumber + '.</div><div class="plTitle">' + trackName + '</div><div class="plLength">' + trackLength + '</div></div></li>');
+            }),
+            trackCount = tracks.length,
+            npTitle = $('#npTitle'),
+            audio = $('#audio1').bind('play', function () {
+                playing = true;
+            }).bind('pause', function () {
+                playing = false;
+            }).bind('ended', function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    audio.play();
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }).get(0),
+            btnPrev = $('#btnPrev').click(function () {
+                if ((index - 1) > -1) {
+                    index--;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            btnNext = $('#btnNext').click(function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            li = $('#plList li').click(function () {
+                var id = parseInt($(this).index());
+                if (id !== index) {
+                    playTrack(id);
+                }
+            }),
+            loadTrack = function (id) {
+                $('.plSel').removeClass('plSel');
+                $('#plList li:eq(' + id + ')').addClass('plSel');
+                npTitle.text(tracks[id].name);
+                index = id;
+                audio.src = mediaPath + tracks[id].file + "?raw=true";
+            },
+            playTrack = function (id) {
+                loadTrack(id);
+                audio.play();
+            };
+        loadTrack(index);
+    }
+});
+
+//initialize plyr
+plyr.setup($('#audio1'), {});
